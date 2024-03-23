00c0d77487de6aa68693d0f50b42bc4185199c11
kost960 <pozitq3@gmail.com>
2024-03-23 22:04:41 +0700

    9 tasks

--- Task 8.py
+++ Task 8.py
@@ -1,10 +1,10 @@
-a = int(input())
-if 1 <= a <= 10:
-    print(a - 1)
-elif 11 <= a <= 190:
-    k = (a+10)/20
-    print(k)
-elif 191 <= a <= 390:
-    pass
-elif 391 <= a <= 590:
-    pass
+number = int(input('Введите число: '))
+
+output_number = list(range(-1, 201))
+
+if number < 10:
+    print(output_number[number])
+elif number < 191:
+    print(str(output_number[(number-9)//2 + 10])[number % 2])
+else:
+    print(str(output_number[(number-188)//3 + 100])[((number - 188) % 3)])
